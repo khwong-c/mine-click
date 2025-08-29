fa3c512f4b49e9e3490dbe7085ef00ae24d67ee6
khwong-c64 <kin.hin.wong.c@gmail.com>
2025-08-30 00:55:47 +0100

    feat: Scale on Press

--- src/components/CircleButton.tsx
+++ src/components/CircleButton.tsx
@@ -1,3 +1,4 @@
+import {motion} from "motion/react"
 import {useState} from "react";
 import GrassBlock from "../images/Grass_Block.png";
 
@@ -16,15 +17,18 @@ export function CircleButton(prop: {
         setPressed(false);
     };
 
-    return <button
-        onClick={onClick}
-        onMouseDown={onPress} onMouseUp={onRelease} onMouseLeave={onRelease}
-        className={`w-24 md:w-32 lg:w-48 h-24 md:h-32 lg:h-48 rounded-full ${isPressed ? 'bg-green-600 scale-95' : 'bg-green-500 hover:scale-105'} flex items-center justify-center shadow-lg hover:bg-green-400 transition-all duration-200 focus:outline-none z-20`}
-        aria-label="Spawn tiles">
-        <div className={`relative w-20 h-20 ${isPressed ? 'scale-95' : ''} transition-transform duration-200`}>
-            <img
-                src={GrassBlock}
-                className="w-full h-full object-contain" draggable="false"/>
-        </div>
-    </button>;
+    return <motion.div className="z-20" whileTap={{scale: 0.8}}>
+        <button
+            onClick={onClick}
+            onMouseDown={onPress} onMouseUp={onRelease} onMouseLeave={onRelease}
+            className={`w-24 md:w-32 lg:w-48 h-24 md:h-32 lg:h-48 rounded-full ${isPressed ? 'bg-gray-500' : 'bg-gray-700 hover:scale-105'} flex items-center justify-center shadow-lg hover:bg-gray-600 transition-all duration-200 focus:outline-none`}
+            aria-label="Spawn tiles">
+            <div className={`relative w-20 h-20 `}>
+                <img
+                    src={GrassBlock}
+                    alt="Press to mine"
+                    className="w-full h-full object-contain" draggable="false"/>
+            </div>
+        </button>
+    </motion.div>;
 }
